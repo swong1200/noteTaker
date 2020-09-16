6aa975cfdbda3d58443fc1f8ae6ce6d6c47b442e
Stuart Wong <swong31@gmail.com>
2020-09-16 13:28:55 -0700

    Serving static files; back end is receiving objects correctly and routing them to the database; brought in npm uuid to add ids to objects

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,52 @@
-module.exports = function(app) {
+// ===============================================================================
+// LOAD DATA
+// We are linking our route to our "data" source.
+// This data sources holds an array with titles and matching text.
+// ===============================================================================
 
+const database = require("../db/db.json");
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+// API GET Requests
+// Below code handles when users "visit" a page.
+// In each of the below cases when a user visits a link
+// (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+// ---------------------------------------------------------------------------
+
+  app.get("/api/notes", function(req, res) {
+    res.json(database);
+  });
+
+// API POST Requests
+// Below code handles when a user submits a form and thus submits data to the server.
+// In each of the below cases, when a user submits form data (a JSON object)
+// ...the JSON is pushed to the appropriate JavaScript array
+// (ex. User fills out a reservation request... this data is then sent to the server...
+// Then the server saves the data to the tableData array)
+// ---------------------------------------------------------------------------
+  app.post("/api/notes", function(req, res) {
+    let notes = req.body;
+    console.log(notes);
+    console.log(uuidv4())
+    database.push(notes);
+    console.log(database);
+    fs.writeFile("db/db.json", JSON.stringify(database), function(err) {
+
+      if (err) {
+        return console.log(err);
+      }
+      res.json(true);
+      console.log("Success!");
+    
+    });
+    
+    
+
+
+  })
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@
 // DEPENDENCIES
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
-var path = require("path");
+const path = require("path");
 
 
 // ===============================================================================
@@ -15,16 +15,13 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
-
+  app.get("/"), function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+  }
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
-
-  // If no matching route is found default to home
+//   If no matching route is found, default to home
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
